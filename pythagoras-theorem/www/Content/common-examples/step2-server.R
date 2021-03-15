ec28da8c6624b9fdfc6481f41695afc404b71360
abdalahshaban <abdalahshaban@yahoo.com>
2021-03-15 18:06:26 +0200

    common two

--- pythagoras-theorem/www/Content/common-examples/step2-server.R
+++ pythagoras-theorem/www/Content/common-examples/step2-server.R
@@ -1,9 +1,24 @@
+color <- "#0049C5"
+cex <- 1
+cm <- tr("cm")
+A <- tr_latex("A")
+B <- tr_latex("B")
+C <- tr_latex("C")
+D <- tr("D")
+quition_mark <- tr_latex("?")
+output[["plot_common_two_image_one"]] <- renderPlot({
+  img <- readPNG("www/deprecated/images/common_two_image_one.png")
+  par(bg = 'transparent')
+  # par(bg = 'green')
+  par(mai = c(0, 0, 0, 0))
+  plot(axes = F, ann = FALSE, xlim = c(0, 110), ylim = c(0, 110), x = NA)
+  rasterImage(image = img, xleft = 5, ybottom = 5, xright = 100, ytop = 100)
+  text(A, x = 95, y = 80, cex = cex, col = color, font = 7)
+  text(B, x = 95, y = 6, cex = cex, col = color, font = 7)
+  text(C, x = 73, y = 6, cex = cex, col = color, font = 7)
+  text(D, x = 6, y = 88, cex = cex, col = color, font = 7)
+  text(paste(con_num(3), cm), x = 85, y = 8, cex = cex, col = color, font = 7)
+  text(paste(con_num(4), cm), x = 99, y = 45, cex = cex, col = color, font = 7, srt = 90)
+  text(paste(con_num(13), cm), x = 50, y = 85, cex = cex, col = color, font = 7)
+}, height = 140, width = 250)
 
-# If you have observers here in this step, you should uncomment the next lines and destroy them
-# If this is the only step in the tab, you should keep this commented
-# onclick("nextBtnCommon",{
-#   observe1$destroy()
-# })
-# onclick("prevBtnCommon",{
-#   observe1$destroy()
-# }) 
--- pythagoras-theorem/www/Content/common-examples/step2.R
+++ pythagoras-theorem/www/Content/common-examples/step2.R
@@ -1,2 +1,137 @@
-div(class = "flex-center-two",
-    h3(translation[[language$current]][["Step two content"]]))
+
+opposite_figure <- tr("opposite_figure")
+A <- tr("A")
+B <- tr("B")
+C <- tr("C")
+D <- tr("D")
+m <- tr("M")
+cm <- tr("cm")
+and <- tr("and")
+AC_overLine <- tr("AC_overLine")
+CD_overLine <- tr("CD_overLine")
+quadrilaterl <- tr("quadrilaterl")
+find_len <- tr("find_len_2")
+find_len_of <- tr("find_len")
+align <- tr_style("text-align")
+
+angle_shape <- withMathJax("\\(^\\circ\\)")
+p_style <- "font-family: Poppins;font-style: normal;font-weight: normal;font-size: 20px;line-height: 30px;"
+
+therefore <- withMathJax("\\(\\therefore\\)")
+because <- withMathJax("\\(\\because\\)")
+triangle_shape <- withMathJax("\\(\\bigtriangleup\\)")
+BC_2 <- tr("BC_2")
+AB_2 <- tr("AB_2")
+AC_2 <- tr("AC_2")
+CD_2 <- tr("CD_2")
+AD_2 <- tr("AD_2")
+Given <- tr("Given")
+R.T.F <- tr("R.T.F")
+Proof <- tr("Proof")
+right_angle_triangle <- tr("right_angle_triangle")
+Pythagoras <- tr("Pythagoras")
+first_req <- tr("first_req")
+second_req <- tr("second_req")
+cm <- tr("cm")
+
+tagList(
+    div(
+        class = "col-xs-12 row-rtl",
+        div(
+            class = "col-xs-10 row-rtl",
+            style = "margin:auto",
+            div(
+                class = "col-xs-6",
+                style = align,
+                style = p_style,
+                p(opposite_figure),
+                p(A, B, C, D, quadrilaterl),
+                p(m, "(<", B, ")", "=", m, "(<", A, C, D, ")", con_num(90), angle_shape),
+                p(",", A, B, "=", con_num(4), cm, ",", B, C, "=", con_num(3), cm, ".", and, A, D, "=", con_num(13), cm, "."),
+                p(find_len_of, AC_overLine, and, CD_overLine)
+            ),
+            div(
+                class = "col-xs-6",
+                plotOutput(outputId = "plot_common_two_image_one")
+            ),
+
+            div(
+                class = "col-xs-12",
+                div(
+                    class = "col-xs-12",
+                    div(
+                        class = "info_msg",
+                        style = "font-size: 16px;line-height: 24px;",
+                        span(class = "fa fa-info-circle"),
+                        div(
+                        class = "msg_content",
+                        style = align,
+                            div(
+                                class = "col-xs-12 row-rtl",
+                                div(
+                                    class = "col-xs-1",
+                                    p(Given, style = "color: #0049C5;")
+                                ),
+                                div(
+                                    class = "col-xs-11",
+                                    p(m, "(<", B, ")", "=", m, "(<", A, C, D, ")", "=", con_num(90), angle_shape, ",", A, B, "=", con_num(4), cm, B, C, "=", con_num(3), cm, ".", and, A, D, "=", con_num(13), cm, ".")
+                                )
+                            ),
+                            div(
+                                class = "col-xs-12 row-rtl",
+                                style = "font-size: 16px",
+                                div(
+                                    class = "col-xs-1",
+                                    p(R.T.F, style = "color: #0049C5;",)
+                                ),
+                                div(
+                                    class = "col-xs-11",
+                                   p(find_len, AC_overLine, and, CD_overLine,)
+                                )
+                            ),
+                            div(
+                                class = "col-xs-12 row-rtl",
+                                div(
+                                    class = "col-xs-1 ",
+                                    p(Proof, style = "color: #0049C5;")
+                                ),
+                                div(
+                                    class = "col-xs-11 proof-p",
+                                    p(because, triangle_shape, A, B, C, right_angle_triangle, B),
+                                    p(therefore, AC_2, "=", AB_2, "+", BC_2, "  ", "(", Pythagoras, ")"),
+                                    p(therefore, AC_2, "=", number_mj(4), "+", number_mj(3), "=", con_num(16), "+", con_num(9), "=", con_num(25)),
+                                    if (language$current == "english") {
+                                      tagList(
+                                          p(class = "col-xs-9", span(therefore, A, C, "=", number_sqrt(25), "=", con_num(5), cm), "."),
+                                          p(class = "col-xs-3", span("(", first_req, ")"))
+                                      )
+                                    } else {
+  tagList(
+                                        p(class = "col-xs-3", span("(", first_req, ")")),
+                                        p(class = "col-xs-9", span(therefore, A, C, "=", number_sqrt(25), "=", con_num(5), cm), ".")
+                                        )
+},
+                                    p(because, triangle_shape, A, C, D, right_angle_triangle, C),
+                                    p(therefore, CD_2, "=", AD_2, "-", AC_2),
+                                    p(therefore, CD_2, "=", number_mj(13), "-", number_mj(5), "=", con_num(169), con_num(25), "=", con_num(144), "  ", "(", Pythagoras, ")"),
+                                   if (language$current == "english") {
+                                    tagList(
+                                        p(class = "col-xs-9", span(therefore, C, D, "=", number_sqrt(144), "=", con_num(12), cm), "."),
+                                        p(class = "col-xs-3", span("(", second_req, ")"))
+                                    )
+                                   } else {
+                                            tagList(
+                                                 p(class = "col-xs-3", span("(", second_req, ")")),
+                                                 p(class = "col-xs-9", span(therefore, C, D, "=", number_sqrt(144), "=", con_num(12), cm), ".")
+                                            )
+}
+                                )
+                            )
+                        )
+                    )
+                )
+            )
+
+        )
+    )
+)
